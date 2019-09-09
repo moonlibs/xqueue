e8ebf70c9dea5466250d70ea5b98681da372c02c
Vladislav Grubov <vladgrubov@gmail.com>
2019-09-09 12:14:53 +0300

    Fixes sleep for 1 second inside worker fiber

--- xqueue.lua
+++ xqueue.lua
@@ -621,7 +621,7 @@ function M.upgrade(space,opts,depth)
 							space:release(task)
 						end
 					end
-					fiber.sleep(1)
+					fiber.yield()
 				end
 				log.info("worker %s ended", i)
 			end,space,self,i)
